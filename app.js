78d26aa82308e011cccf953d66e7b2269136c0c6
Christal Bell <christalvictoriabell@gmail.com>
2023-05-19 20:30:43 -0700

    js take 2 still not functioning

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
 const onSubmit = () => {
-    const firstName = document.getElementById("first-name");
-    const lastName = document.getElementById("last-name");
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
+    const info = [firstName, lastName, email, password];
 
-    const inputs = [firstName, lastName, email, password];
-
-    inputs.forEach((input => {
-        if (input.value === ""){
+    inputs.forEach((input) => {
+        if (input.value === "") {
             input.classList.add("error");
-        }
-    }
-    ));
-   
-};
+        } 
+
+    })
+}
+
+
