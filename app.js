76ee62b3b027dd6441cff2f958b62b1b33e1cf98
Christal Bell <christalvictoriabell@gmail.com>
2023-05-19 19:28:34 -0700

    take 1 onclick still not functioning

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const onSubmit = () => {
-    const firstName= document.getElementById("first-name");
-    const lastName= document.getElementById("last-name");
-    const email= document.getElementById("email");
-    const password= document.getElementById("password");
+    const firstName = document.getElementById("first-name");
+    const lastName = document.getElementById("last-name");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
 
-    const inputs = [firstName, lastName, email, password]; 
+    const inputs = [firstName, lastName, email, password];
 
-        inputs.forEach((input) => {
-            if (input.value === "") {
-                input.classList.add("error");
-            }
-        });
-
+    inputs.forEach((input => {
+        if (input.value === ""){
+            input.classList.add("error");
+        }
+    }
+    ));
+   
 };
